baba64204014f06ac8d6737611d244acd206d2eb
Nomura <guilherme.leite@sensedata.com.br>
2021-08-27 18:30:27 -0300

    func js

--- function.js
+++ function.js
@@ -1,11 +1,15 @@
-const number = prompt('Digite um número inteiro: ');
+function soma_divisores() {
+    const number = prompt('Digite um número inteiro: ');
 
-var sum = 0;
+    var sum = 0;
 
-for(var i = 3; i < number; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-        sum += i;
+    for(var i = 3; i < number; i++){
+        if(i % 3 == 0 || i % 5 == 0){
+            sum += i;
+        }
     }
+
+    console.log(`A soma resultante é: ${sum}`)
 }
 
-console.log(`A soma resultante é: ${sum}`)
+soma_divisores()
